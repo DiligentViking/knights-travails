4ebbb304acfdbe651e729ccb1f2cf8c5f2dc9952
DiligentViking <acore-n@proton.me>
2026-01-14 14:56:50 -0600

    Patch and polish solution; Compare with another's
    My solution had better time efficiency than the other solution.
    
    Also, I added/changed a few lines on the other solution.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,3 +8,31 @@ console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
 for (const move of path.toReversed()) {
   console.log(`[${move[0]},${move[1]}]`);
 }
+
+
+// ----------
+// Racing:
+
+console.log('\n----------\nRacing:\n\n');
+
+function race100(func1, func2, ...args) {
+  console.time(func1.name);
+  for (let i = 0; i < 100; i++) func1(...args);
+  console.timeEnd(func1.name);
+  console.time(func2.name);
+  for (let i = 0; i < 100; i++) func2(...args);
+  console.timeEnd(func2.name);
+}
+
+import { knightMoves2 } from "./other's_solutions/sagar-shrigadi..graph";
+
+race100(knightMoves, knightMoves2, [0, 0], [1, 2]);
+race100(knightMoves, knightMoves2, [0, 0], [7, 7]);  // Won 20 out of 23
+
+// console.log('\n');
+// import { knightMoves2 } from "./other's_solutions/sagar-shrigadi..graph";
+
+// race100(knightMoves, knightMoves2, [0, 0], [1, 2]);
+// race100(knightMoves, knightMoves2, [0, 0], [7, 7]);  // Won 20 out of 23
+
+console.log(knightMoves2([0, 0], [7, 7]));
--- src/scripts/knight-moves.js
+++ src/scripts/knight-moves.js
@@ -4,32 +4,28 @@ export function knightMoves(start, end) {
   let pos = start;
   let i = 0;
   while (true) {
-    if (i > 1000) {
-      console.log('FIREBREAK');
-      break;
-    }
     // Find possible positions from current positions
     const [posX, posY] = pos;
     const mapKey = posX.toString() + posY.toString();
-    map[mapKey] = 0;
-    const moves = [[posX-1, posY+2], [posX+1, posY+2], [posX+2, posY+1], [posX+2, posY-1], [posX+1, posY-2], [posX-1, posY+2], [posX-2, posY-1], [posX-2, posY+1]];
+    map[mapKey] = 1;
+    const moves = [[posX-1, posY+2], [posX+1, posY+2], [posX+2, posY+1], [posX+2, posY-1], [posX+1, posY-2], [posX-1, posY-2], [posX-2, posY-1], [posX-2, posY+1]];
     // Add valid positions
     for (const [x, y] of moves) {
-      if (x < 0 || x > 7 || y < 0 || y > 7) {
-        continue;
-      }
       if (map[`${x}${y}`]) {
         continue;
       }
+      if (x < 0 || x > 7 || y < 0 || y > 7) {
+        continue;
+      }
       queue.push({coord: [x, y], parent: i});
-      map[mapKey]++;
       // Check if end position found, then return path
       if (x === end[0] && y === end[1]) {
         const path = [];
         path.push([x, y]);
-        while (queue[i].parent !== null) {
-          path.push(queue[i].coord);
-          i = queue[i].parent;
+        let p = i;
+        while (queue[p].parent !== null) {
+          path.push(queue[p].coord);
+          p = queue[p].parent;
         }
         path.push(start);
         return path;
